e200ef292c3550765cf10f7dc6afd14714993fe3
Pogo86 <ellinson86@gmail.com>
2024-03-20 14:09:32 +0000

    Merge csv program
    Created a program to merge any number of CSV files

---
+++ dataAnalysis/mergeCSV/mergeCSV.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+import glob
+
+inputDataLocation = '/Users/rich/git/pythonScripts/dataAnalysis/inputData'
+
+joinedFiles = os.path.join(inputDataLocation, '*.csv')
+
+joinedList = glob.glob(joinedFiles)
+
+df = pd.concat(map(pd.read_csv, joinedList), ignore_index=True)
+df.to_csv('/Users/rich/git/pythonScripts/dataAnalysis/mergeCSV/merged.csv', index=False)
+
+print(f'{len(joinedList)} Files Joined')
